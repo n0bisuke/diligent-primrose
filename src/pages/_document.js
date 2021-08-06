047bafe173efb22081123acf08f7775b42aaca41
n0bisuke <rsksound@gmail.com>
2021-08-06 12:34:28 +0900

    ga

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,7 +32,22 @@ export default class MyDocument extends Document {
         // const pageProps = _.get(this.props, '__NEXT_DATA__.props.pageProps');
         return (
             <Html {...this.helmetHtmlAttrComponents}>
-                <Head>{this.helmetHeadComponents}</Head>
+                <Head>
+                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}/>
+                    <script
+                        dangerouslySetInnerHTML={{
+                        __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${GA_TRACKING_ID}', {
+                        page_path: window.location.pathname,
+                        });
+                    `,
+                        }}
+                    />
+                    {this.helmetHeadComponents}
+                </Head>
                 <body {...this.helmetBodyAttrComponents}>
                     <Main />
                     <script src={withPrefix('js/plugins.js')} />
