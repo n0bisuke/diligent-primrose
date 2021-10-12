ae87378fe111cd48765e39220eb8afb1524c8769
n0bisuke <rsksound@gmail.com>
2021-10-13 01:22:23 +0900

    add oc

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -32,6 +32,7 @@ export default class Post extends React.Component {
                         </div>
                     )}
                     {markdownContent && <div className="post-content inner-sm">{markdownify(markdownContent)}</div>}
+                    
                     <footer className="post-meta inner-sm">
                         <time className="published" dateTime={dateTimeAttr}>{formattedDate}</time>
                     </footer>
