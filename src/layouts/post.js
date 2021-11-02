1678778e4818d7d922ed7e06e1199f9b2d65660b
Stackbit Code Editor <projects@stackbit.com>
2021-11-02 05:39:43 +0000

    post.js: changed

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -33,6 +33,7 @@ export default class Post extends React.Component {
                     )}
 
                     <script type="text/javascript">amzn_assoc_ad_type ="responsive_search_widget"; amzn_assoc_tracking_id ="n0bisuke-22"; amzn_assoc_marketplace ="amazon"; amzn_assoc_region ="JP"; amzn_assoc_placement =""; amzn_assoc_search_type = "search_widget";amzn_assoc_width ="auto"; amzn_assoc_height ="auto"; amzn_assoc_default_search_category =""; amzn_assoc_default_search_key ="";amzn_assoc_theme ="light"; amzn_assoc_bg_color ="FFFFFF"; </script><script src="//z-fe.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1&Marketplace=JP"></script>
+                    
                     {markdownContent && <div className="post-content inner-sm">{markdownify(markdownContent)}</div>}
                     <footer className="post-meta inner-sm">
                         <time className="published" dateTime={dateTimeAttr}>{formattedDate}</time>
